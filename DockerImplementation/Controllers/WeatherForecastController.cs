1b604172f35770e27f3875854dbad36715dfa3e4
nathanvitus <vitusnathan@outlook.com>
2025-07-29 21:59:57 +0530

    Docker file updated

--- DockerImplementation/Controllers/WeatherForecastController.cs
+++ DockerImplementation/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace DockerImplementation.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezinggly", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
